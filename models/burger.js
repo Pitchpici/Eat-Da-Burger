f0427e7ebde73261b870b9ece8459d9c34f47db9
Roxana Milea <roxanamilea@ymail.com>
2018-09-16 14:55:54 -0400

    corrected orm.js again

---
+++ models/burger.js
@@ -0,0 +1,24 @@
+const orm = require("./config/orm.js");
+
+const burger = {
+	all: function(cb) {
+		orm.selectAll("burgers", function(res) {
+			cb(res);
+		});
+	},
+
+	insert: function(name, cb) {
+		orm.insertOne("burgers", 
+			["burger_name", "devoured"],
+			[name, false],
+			cb)
+	}, 
+
+	update: function(id, cb) {
+		let condition = "id=" + id;
+
+		orm.updateOne("burgers", {devoured: true}, condition, cb); 
+	};
+};
+
+module.exports = burger;
