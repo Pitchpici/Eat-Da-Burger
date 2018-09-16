1ba0b9cc9abf77ae584f99b968fcc7d74f5d8564
Roxana Milea <roxanamilea@ymail.com>
2018-09-15 22:42:18 -0400

    corrected orm.js

--- config/orm.js
+++ config/orm.js
@@ -2,16 +2,16 @@ const connection = require("./connection");
 
 var orm = {
 
-  selectAll: function() {
-    var queryString = "SELECT * FROM burgers";
+  selectAll: function(table, cb) {
+    var queryString = "SELECT * FROM " + table + ";";
 
     connection.query(queryString, function(err, result) {
       if (err) throw err;
-      console.log(result);
+      cb(result);
     });
   },
 
-  insertOne: function(burgerName, devoured) {
+  insertOne: function(burgerName, devoured, cb) {
     var queryString = "INSERT INTO burgers SET ?";
     console.log(queryString);
 
@@ -20,12 +20,14 @@ var orm = {
     	devoured: devoured
     }], function(err, result) {
       if (err) throw err;
-      console.log(result);
+
+      cb(result);
     });
   },
 
-  updateOne: function(burgerName, devoured) {
+  updateOne: function(burgerName, devoured, cb) {
     var queryString = "UPDATE burger SET ? WHERE ?";
+    console.log(queryString);
 
     connection.query(
       queryString,
@@ -37,7 +39,7 @@ var orm = {
       }],
       function(err, result) {
         if (err) throw err;
-        console.log(result);
+        cb(result);
       }
     );
   }
